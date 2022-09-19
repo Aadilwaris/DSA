a0a64882fe62dc0ad90df97464db1911f5ac2df7
Adil Waris <101867676+Aadilwaris@users.noreply.github.com>
2022-09-20 03:07:07 +0530

    Update QuickSort.java

--- Algorithms/QuickSort.java
+++ Algorithms/QuickSort.java
@@ -1,9 +1,6 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+//Partion funtion
 
-public class Main
-{      public static int partition(int a[],int l,int h){
+public static int partition(int a[],int l,int h){
                 int pivot=a[h];
                 int i=l-1;
                 for(int j=l;j<h;j++){
@@ -20,25 +17,13 @@ public class Main
                 a[i]=temp;
                 return i;
 }
-        public static void quickSort(int a[],int l,int h){
+
+//Quick Sort function
+
+public static void quickSort(int a[],int l,int h){
                 if(l<h){
                         int pivot = partition(a,l,h);
                         quickSort(a,l,pivot-1);
                         quickSort(a,pivot+1,h);
                 }
-}
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		//your code here
-                Scanner sc = new Scanner(System.in);
-                int n=sc.nextInt();
-                int a[]=new int[n];
-                for(int i=0;i<n;i++){
-                        a[i]=sc.nextInt();
-                }
-                quickSort(a,0,n-1);
-                for(int i=0;i<n;i++){
-                        System.out.print(a[i]+" ");
-                }
-	}
 }
