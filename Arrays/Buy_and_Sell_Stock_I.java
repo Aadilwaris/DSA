51c081029e7513a750e1c17e8de60e87dff7770a
Adil Waris <101867676+Aadilwaris@users.noreply.github.com>
2022-10-09 13:43:20 +0530

    Create Buy_and_Sell_Stock_I.java

---
+++ Arrays/Buy_and_Sell_Stock_I.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy=Integer.MAX_VALUE;
+        int sell = Integer.MIN_VALUE;
+        for(int i=0;i<prices.length;i++){
+          buy=Math.min(buy, prices[i]);
+          sell=Math.max(sell,prices[i]-buy);
+        }
+        return sell;
+    }
+}
