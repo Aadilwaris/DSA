4abf9740dc321f60df8cc754007a7dee477129b6
Adil Waris <101867676+Aadilwaris@users.noreply.github.com>
2022-10-12 01:36:24 +0530

    Create Move_Zeroes.java

---
+++ Arrays/Move_Zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+       int n=nums.length;
+       int j=0;
+       for(int i=0;i<n;i++){
+           if(nums[i]!=0){
+               nums[j]=nums[i];
+               j++;
+           }
+       }
+       while(j<n){
+           nums[j]=0;
+           j++;
+       }
+    }
+}
